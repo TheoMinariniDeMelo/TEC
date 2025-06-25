ce94d855f8dc3dd0ab79b5e0c6137dba486b2316
TheoMinariniDeMelo <theominarini@outlook.com>
2025-06-25 17:48:42 -0300

    chore(comment): comment in main has been updated

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "./terminal_config.c"
+
 int main(int argc, char** argv){
 	if(argc != 2){
 		printf("Enter with a filename:");
@@ -20,6 +21,9 @@ int main(int argc, char** argv){
 		 * algo inesperado ocorre, diferente do retorno padrão como em malloc ou read/write,
 		 * ele descreve comportamentos, como descritor de arquivo em estado ruim ou 
 		 * recurso não disponível (EAGAIN), todos os valores são inteiros positivos
+		 * Nesta parte há a validação do errno pois o programa está em raw mode, portanto
+		 * não espera a entrada de um dado pra retorno da função read, neste caso ela retorna -1
+		 * e define errno = EAGIN, se não definir, significa que a função realmente falhou
 		 * */
 		if(read(STDIN_FILENO, &c, 1) != -1 && errno != EAGAIN){
 
